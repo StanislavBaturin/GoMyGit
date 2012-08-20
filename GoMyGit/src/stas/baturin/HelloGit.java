6e29a1cc011dbafea2c1bbf664214d122cb64f7b
Stanislav <bat5738@gmail.com>
2012-08-20 19:32:30 +0400

    Git

---
+++ GoMyGit/src/stas/baturin/HelloGit.java
@@ -0,0 +1,14 @@
+package stas.baturin;
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git what to Fuck ??????");
+
+	}
+
+}
